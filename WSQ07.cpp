104ce1e0b5783446591544f321df3906a83be45a
samir96 <A01630155@itesm.mx>
2015-02-27 14:59:39 -0600

    WSQ
    Here I will upload all my WSQ

---
+++ WSQ07.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+  Minimun, Max, Sum, SumaT; int
+cout << "This programa makes the sum of a range of number "<<endl;
+cout<< "Give me the Minumun numer of your range "
+cin>> Minimun;
+cout<<" Give me the Maximum number of your range "
+cin>> Max;
+
+while{(Max-1=Min)
+  Sum=Max-1;
+  SumaT=Max+Sum;
+cout<<" The sum of your range is: " <<SumaT<<;
+}
+return 0;
+}
